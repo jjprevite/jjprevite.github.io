37647a6569de6ccd43240d1ea894f20809a7b70b
Joe P <jjprevite@gmail.com>
2017-06-24 19:12:18 +0200

    Add blog posts for Patatap and Success Story Machine Make small changes to index page

--- projects/successStoryMachine/main.js
+++ projects/successStoryMachine/main.js
@@ -1,4 +1,4 @@
-var title = document.getElementById("story-title");
+cvar title = document.getElementById("story-title");
 var link = document.getElementById("story-link");
 var button = document.querySelector("#getStory");
 var tweet = document.getElementById("tweet");
