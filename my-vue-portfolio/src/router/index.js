c80e9a0498b3c49ce202a81b9a1187af7bf35fec
Joe P <jjprevite@gmail.com>
2017-10-01 14:31:37 -0700

    Add route for Blog Post

--- my-vue-portfolio/src/router/index.js
+++ my-vue-portfolio/src/router/index.js
@@ -1,12 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from '@/components/Home'
+import BlogPost from '@/components/BlogPost'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/'
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/blog',
+      name: 'BlogPost',
+      component: BlogPost
     }
-  ]
+  ],
+  mode: 'history'
 })
