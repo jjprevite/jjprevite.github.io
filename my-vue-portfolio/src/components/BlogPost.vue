dd10d400e19101dacada38b4032705dd73aef53b
Joe P <jjprevite@gmail.com>
2017-10-01 14:30:03 -0700

    Create Blog Post component

---
+++ my-vue-portfolio/src/components/BlogPost.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="single-blog-post">
+        <h3>I'm inside.</h3>
+        <h1>{{ post.title }}</h1>
+        <article v-for="post in post.posts">{{ post.content }}</article>
+        <figure>
+            <img src="post.image">
+        </figure>
+        </div>
+    </div>
+</template>
+
+<script>
+// import postData from '../assets/posts.json'
+
+export default {
+  data () {
+    return {
+      id: this.$route.params.id,
+      post: {}
+    }
+  },
+  created () {
+    console.log(this.id)
+  }
+}
+
+</script>
+
+<style>
+
+</style>
