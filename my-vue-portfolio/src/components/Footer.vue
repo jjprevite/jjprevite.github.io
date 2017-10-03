62f888b7348afb62b21b7cd57a15e32c5bb904a6
Joe P <jjprevite@gmail.com>
2017-10-03 11:44:20 -0700

    Change fa icons size

--- my-vue-portfolio/src/components/Footer.vue
+++ my-vue-portfolio/src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
 	<b-container>
-		<h5 class="footer-text">Made with <icon name="music"></icon> and <icon name="heart"></icon> in Arizona by Joe Previte &copy; {{ date }}</h5>
+		<h5 class="footer-text">Made with <icon name="music" style="font-size:em;"></icon> and <icon name="heart"></icon> in Arizona by Joe Previte &copy; {{ date }}</h5>
 </b-container>
 </template>
 
@@ -18,10 +18,19 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-	.footer-text {
-		font-size: 0.8em;
-	}
+.footer-text {
+	font-size: 0.8em;
+}
 
+.fa-icon {
+  width: auto;
+  height: 0.8em; /* or any other relative font sizes */
+  margin: 0 2px;
+
+  /* You would have to include the following two lines to make this work in Safari */
+  max-width: 100%;
+  max-height: 100%;
+}
 	
 
 </style>
