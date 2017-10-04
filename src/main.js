e22c4321bef737c1408eba27e81c7d175b6adcd0
Joe P <jjprevite@gmail.com>
2017-10-04 16:00:10 -0700

    load bootstrap css first

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/css/bootstrap.css'
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import App from './App'
 import router from './router'
 import 'vue-awesome/icons'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import 'bootstrap/dist/css/bootstrap.css'
 import Icon from 'vue-awesome/components/Icon'
 import VModal from 'vue-js-modal'
 
